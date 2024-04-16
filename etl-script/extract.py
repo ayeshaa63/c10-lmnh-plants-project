ab530b0453031c6c63db117c11caac4623e6d9c9
ayesha <ayeshaparvez0603@gmail.com>
2024-04-16 09:09:51 +0100

    initial extract script

--- etl-script/extract.py
+++ etl-script/extract.py
@@ -2,12 +2,28 @@
 The data should then be returned as a Dataframe."""
 
 import pandas as pd
+import requests
 
 
-def extract() -> pd.DataFrame:
-    """main extraction function"""
-    return None
+def extract(plant_range: int) -> list:
+
+    plants = []
+
+    for i in range(plant_range):
+        response = requests.get(
+            f"https://data-eng-plants-api.herokuapp.com/plants/{i}")
+
+        plant = response.json()
+
+        plants.append(plant)
+
+    return plants
+
+
+def to_dataframe():
+    pass
 
 
 if __name__ == "__main__":
-    extract()
+
+    print(extract())
