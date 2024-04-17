63b9b41b6e1fc58f3ba83f1431d75f9700a394ae
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-17 15:40:16 +0100

    init commit

--- etl-script/test_load.py
+++ etl-script/test_load.py
@@ -1,4 +1,6 @@
 """Test the functions involved in the load file."""
+from unittest.mock import patch
+from load import get_botanist_ids, check_plants, upload_watering_data, upload_recordings_data, load
 
 
 def test_load():
