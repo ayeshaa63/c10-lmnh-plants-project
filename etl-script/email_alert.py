2852699fa8339a5fbf293f12c1c3ffcf1a2fc00b
NMcKittie <nathankit66@gmail.com>
2024-04-17 12:51:34 +0100

    added plant name to tranform.py and added an email alert for sensor failure

---
+++ etl-script/email_alert.py
@@ -0,0 +1,70 @@
+"""send an email to users outlining any plants where theres been sensor failures"""
+
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime
+from os import environ as ENV
+from dotenv import load_dotenv
+
+
+def create_message(sensor_failures: list[str]) -> str:
+
+    message = """<h1 style="font-weight: bold;text-decoration: underline;">Sensor Failures</h1>"""
+    for failure in sensor_failures:
+        message += f"""<h3>{failure}</h3> """
+
+    message = message.replace("\n", "")
+
+    return message
+
+
+def get_to_emails():
+
+    nathan_email = ENV['NATHAN_EMAIL']
+    dana_email = ENV['DANA_EMAIL']
+    howard_email = ENV['HOWARD_EMAIL']
+    ayesha_email = ENV['AYESHA_EMAIL']
+
+    return [nathan_email, dana_email, howard_email, ayesha_email]
+
+
+def send_email(sensor_failures: list[str]):
+    """sends an email report of plant sensor faults using AWS SES"""
+    current_datetime = datetime.now()
+    ses = boto3.client('ses', region_name='eu-west-2')
+    from_email = ENV['NATHAN_EMAIL']
+
+    message = create_message(sensor_failures)
+    to_emails = get_to_emails()
+    subject = f"Sensor Failures @{current_datetime}"
+
+    try:
+        response = ses.send_email(
+            Destination={
+                'ToAddresses': to_emails,
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': 'UTF-8',
+                        'Data': f'{message}',
+                    },
+                },
+                'Subject': {
+                    'Charset': 'UTF-8',
+                    'Data': f"{subject}",
+                },
+            },
+            Source=from_email,
+        )
+    except ClientError as e:
+        print("Error sending email:", e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:", response['MessageId'])
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    msg = ['ERROR :: PLANT 3 :: plant sensor fault ::',
+           'ERROR :: PLANT 8 :: plant sensor fault ::']
+    send_email(msg)
