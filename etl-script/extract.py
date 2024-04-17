2e6eae778295b238e00609da68d4df8ea3296592
NMcKittie <nathankit66@gmail.com>
2024-04-17 11:07:15 +0100

    Added better error checking to transform but does not send email yet. Added print statements to time transform.py and extract.py, they are contained within their respective __main__

--- etl-script/extract.py
+++ etl-script/extract.py
@@ -3,6 +3,7 @@ The data should then be returned as a list of Dictionaries."""
 
 import asyncio
 import aiohttp
+import time
 
 
 async def get_plant_data(session, plant_id: int) -> dict:
@@ -20,7 +21,7 @@ async def get_plant_data(session, plant_id: int) -> dict:
 
     except Exception as e:
         return {'error': 'Cannot connect to the API.',
-                'exception': e}
+                'exception': e, 'plant_id': plant_id}
 
 
 async def get_all_plants(no_of_plants: int) -> list[dict]:
@@ -37,4 +38,6 @@ async def get_all_plants(no_of_plants: int) -> list[dict]:
 
 if __name__ == "__main__":
 
+    start_time = time.time()
     resp = asyncio.run(get_all_plants(51))
+    print(f"--- {(time.time() - start_time)} seconds taken ---")
