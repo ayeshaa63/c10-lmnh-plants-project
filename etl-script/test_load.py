7df4bb983d670c4d2a0954f5c8a625d907f6341f
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-17 18:00:44 +0100

    pytest

--- etl-script/test_load.py
+++ etl-script/test_load.py
@@ -1,21 +1,16 @@
 """Test the functions involved in the load file."""
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import pandas as pd
 from load import get_botanist_ids, check_plants, upload_watering_data, upload_recordings_data, load
 
 
-@patch("load.connect_to_db")
-def test_load(fake_db_connection):
-    """A test to pass the workflow checks (for now)"""
-    fake_db_connection.return_value = None
-    fake_db_connection.return_value.commit.return_value = None
-    fake_db_connection.return_value.cursor.return_value = None
-    fake_db_connection.return_value.cursor.return_value.execute.return_value = None
-    fake_db_connection.return_value.cursor.return_value.fetchone.return_value = {
-        'botanist_id': 1}
+def test_Call_sql():
+    mock_connect = MagicMock()
+    cursor = mock_connect.cursor.return_value
+    cursor.execute.return_value.fetchone.side_effect = {'botanist_id': 1}
     fake_environ = {'SCHEMA_NAME': 'test_schema'}
     test_return = get_botanist_ids(pd.DataFrame({'name': ['mickey', 'mouse'], 'phone': [
-        '8437234', '0-09280'], 'email': ['87324r832@gmail.com', 'mickeymouse@clubhouse.com']}), fake_db_connection, fake_environ)
+        '8437234', '0-09280'], 'email': ['87324r832@gmail.com', 'mickeymouse@clubhouse.com']}), mock_connect, fake_environ)
     assert 'botanist_id' in test_return
     assert isinstance(test_return['botanist_id'].values[0], int)
     assert test_return == pd.DataFrame({'name': ['mickey', 'mouse'], 'phone': [
