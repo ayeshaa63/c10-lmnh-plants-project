b6558a3984579dc46201b2302831835a48429d05
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-17 15:02:33 +0100

    insert missing metadata script

---
+++ etl-script/insert_missing_metadata.py
@@ -0,0 +1,175 @@
+"""A script to insert missing metadata into the database"""
+
+import requests
+from os import environ as ENV
+from pymssql import connect, Connection
+from dotenv import load_dotenv
+from extract import get_plant_data
+
+
+def connect_to_db(config):
+    """Returns a live database connection."""
+    return connect(
+        server=config["DB_HOST"],
+        user=config["DB_USER"],
+        password=config["DB_PASS"],
+        database=config["DB_NAME"],
+        port=config["DB_PORT"],
+        as_dict=True
+    )
+
+
+def get_plant_data(plant_id: int) -> dict:
+    '''Extracts json data from API endpoint for given plant id.'''
+
+    try:
+        response = requests.get(
+            f"https://data-eng-plants-api.herokuapp.com/plants/{plant_id}", timeout=5)
+
+        data = response.json()
+        data['plant_id'] = plant_id
+        return data
+
+    except Exception as e:
+        return {'error': 'Cannot connect to the API.',
+                'exception': e, 'plant_id': plant_id}
+
+
+def insert_continent(continent: str, conn: Connection, config):
+    """Insert data into the continent table of the database,
+    if it doesn't already exist.
+    Return the continent id."""
+    with conn.cursor() as cur:
+        cur.execute(
+            f"""SELECT continent_id from {config['SCHEMA_NAME']}.continent
+                    WHERE name = '{continent}'
+                    """)
+        continent_id = cur.fetchone()
+        if not continent_id:
+            cur.execute(f"""INSERT INTO {config['SCHEMA_NAME']}.continent
+                        (name) VALUES
+                        ('{continent}')""")
+            conn.commit()
+            cur.execute(f"""SELECT continent_id FROM {config['SCHEMA_NAME']}.continent
+                        WHERE name = '{continent}'
+                        """)
+            continent_id = cur.fetchone()
+    return continent_id['continent_id']
+
+
+def insert_country(country: str, continent_id: int, conn, config):
+    """Insert data into the country table of the database,
+    if it doesn't already exist.
+    Return the country id."""
+    with conn.cursor() as cur:
+        cur.execute(
+            f"""SELECT country_id from {config['SCHEMA_NAME']}.country
+                    WHERE name = '{country}' 
+                    AND continent_id = {continent_id}
+                    """)
+        country_id = cur.fetchone()
+        if not country_id:
+            cur.execute(f"""INSERT INTO {config['SCHEMA_NAME']}.country
+                        (name, continent_id) VALUES
+                        ('{country}', {continent_id})""")
+            conn.commit()
+            cur.execute(f"""SELECT country_id FROM {config['SCHEMA_NAME']}.country
+                        WHERE name = '{country}'
+                        AND continent_id = {continent_id}
+                        """)
+            country_id = cur.fetchone()
+    return country_id['country_id']
+
+
+def insert_origin(origin: list, conn: Connection, config):
+    """Insert data into the origin table of the database,
+    if it doesn't already exist.
+    Return the origin id."""
+    long, lat, location, country, continent = origin
+    continent = continent.split('/')[-1]
+    location = location.replace('\'', '\"')
+    continent_id = insert_continent(continent, conn, config)
+    country_id = insert_country(country, continent_id, conn, config)
+    with conn.cursor() as cur:
+        cur.execute(
+            f"""SELECT origin_id from {config['SCHEMA_NAME']}.origin
+                    WHERE long = {long}
+                        AND lat = {lat}
+                        AND location_name = '{location}'
+                        AND country_id = {country_id}
+                    """)
+        origin_id = cur.fetchone()
+        if not origin_id:
+            cur.execute(f"""INSERT INTO {config['SCHEMA_NAME']}.origin
+                        (long, lat, location_name, country_id) VALUES
+                        ({long}, {lat}, '{location}', {country_id})
+                        """)
+            conn.commit()
+            cur.execute(f"""SELECT origin_id FROM {config['SCHEMA_NAME']}.origin
+                        WHERE long = {long}
+                        AND lat = {lat}
+                        AND location_name = '{location}'
+                        AND country_id = {country_id}
+                        """)
+            origin_id = cur.fetchone()
+    return origin_id['origin_id']
+
+
+def insert_plant(data: dict, conn: Connection, config, origin_id: int):
+    """Insert data for a plant into the plant table of the database."""
+    plant_id = data['plant_id']
+    name = data['name'].replace('\'', '\"')
+    if 'scientific_name' in data:
+        scientific_name = data['scientific_name'][0].replace('\'', '\"')
+    else:
+        scientific_name = name
+    with conn.cursor() as cur:
+        cur.execute(f"""INSERT INTO {config['SCHEMA_NAME']}.plant
+                        (plant_id, name, scientific_name, origin_id) VALUES
+                        ({plant_id}, '{name}', '{scientific_name}', {origin_id})
+                        """)
+        conn.commit()
+
+
+def insert_images():
+    """Insert data about the images of a plant if exists and upload to database."""
+    return None
+
+
+def insert_botanist(botanist: dict, conn: Connection, config) -> None:
+    """Insert data into the botanist table of the database."""
+    name, phone, email = botanist['name'], botanist['phone'], botanist['email']
+    with conn.cursor() as cur:
+        cur.execute(f"""SELECT * from {config['SCHEMA_NAME']}.botanist
+                    WHERE name='{name}'
+                    AND phone='{phone}'
+                    AND email='{email}'""")
+        botanist_info = cur.fetchone()
+        if not botanist_info:
+            cur.execute(f"""INSERT INTO {config['SCHEMA_NAME']}.botanist
+                        (name, phone, email) VALUES
+                        ('{name}', '{phone}', '{email}')""")
+            conn.commit()
+
+
+def insert_plant_data(plant_dict: dict, conn: Connection, config):
+    """Insert data into all relevant tables of the database for a single plant."""
+    origin_id = insert_origin(plant_dict['origin_location'], conn, config)
+    insert_plant(plant_dict, conn, config, origin_id)
+    insert_images()
+    insert_botanist(plant_dict['botanist'], conn, config)
+
+
+def insert_missing_plant(plant_id, config):
+    plant = get_plant_data(plant_id)
+    if plant:
+        with connect_to_db(config) as conn:
+            insert_plant_data(plant, conn, config)
+            return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    insert_missing_plant(45, ENV)
--- etl-script/load.py
+++ etl-script/load.py
@@ -1,10 +1,17 @@
 """Take the Dataframe returned from transform.py and
 upload it to a Microsoft SQL Server database."""
 
+import asyncio
+import aiohttp
+import time
 import pandas as pd
 from pymssql import connect, Connection
 from os import environ as ENV
 from dotenv import load_dotenv
+
+from extract import get_all_plants
+from transform import transform
+from insert_missing_metadata import insert_missing_plant
 
 
 def connect_to_db(config):
@@ -24,7 +31,7 @@ def get_botanist_ids(df: pd.DataFrame, conn: Connection, config) -> pd.DataFrame
      - If botanist doesn't exist, add them to database and get id,
      - If botanist does already exist, get their id as appear in table.
      Replace botanist columns in DataFrame to a column of ids."""
-    botanists = df[['name', 'phone', 'email']].drop_duplicates()
+    botanists = df[['name', 'phone', 'email']]
     ids = []
 
     with conn.cursor() as cur:
@@ -47,75 +54,87 @@ def get_botanist_ids(df: pd.DataFrame, conn: Connection, config) -> pd.DataFrame
                     AND phone='{botanist.iloc[1]}'
                     AND email='{botanist.iloc[2]}'""")
                 botanist_info = cur.fetchone()
+
             ids.append(botanist_info['botanist_id'])
-    df['botanist_id'] = pd.Series(ids)
+    df['botanist_id'] = pd.Series(ids, dtype=int)
     return df
 
 
-def upload_watering_data(df: pd.DataFrame, conn: Connection, config):
+def check_plants(df: pd.DataFrame, conn: Connection, config):
+    """Insert data for a plant into the plant table of the database."""
+    plants = df[[
+        "plant_id",
+        "scientific_name",
+        "soil_moisture",
+        "temp"]]
+    bad_plants = []
+    with conn.cursor() as cur:
+        for _, plant in plants.iterrows():
+            plant_id = plant.iloc[0]
+            cur.execute(
+                f"""SELECT * from {config['SCHEMA_NAME']}.plant
+                    WHERE plant_id = {plant_id}
+                    """)
+            plant_info = cur.fetchone()
+            if not plant_info:
+                try:
+                    insert_missing_plant(plant_id, ENV)
+                except:
+                    bad_plants.append(plant_id)
+    return bad_plants
+
+
+def upload_watering_data(df: pd.DataFrame, conn: Connection, config, bad_plants):
     """Upload new watering data to database."""
-    # Not sure if to_sql will work yet, use INSERT INTO if not.
     waterings = df[['last_watered', 'plant_id']]
     with conn.cursor() as cur:
         for _, watering in waterings.iterrows():
-            cmd = f"""IF NOT EXISTS (SELECT timestamp, plant_id
+            if watering.iloc[1] not in bad_plants:
+                cur.execute(f"""IF NOT EXISTS (SELECT timestamp, plant_id
                         FROM {config['SCHEMA_NAME']}.watering
-                        WHERE timestamp = CAST('{watering.iloc[0]}' AS TIMESTAMP)
-                        AND plant_id = {watering.iloc[1]})
+                        WHERE timestamp = '{watering.iloc[0]}'
+                        AND plant_id = {int(watering.iloc[1])})
                         BEGIN
                         INSERT INTO {config['SCHEMA_NAME']}.watering
                         (timestamp, plant_id) VALUES
-                        (CAST('{watering.iloc[0]}' AS TIMESTAMP), {watering.iloc[1]})
-                        END"""
-            print(cmd)
-            cur.execute(cmd)
-            # f"""INSERT INTO {config['SCHEMA_NAME']}.watering
-            #     (timestamp, plant_id) VALUES
-            #     ('{watering.iloc[0]}', '{watering.iloc[1]}')
-            #     WHERE NOT EXISTS (
-            #     SELECT timestamp, plant_id FROM {config['SCHEMA_NAME']}.watering
-            #     WHERE timestamp = {watering.iloc[0]}
-            #     AND plant_id = {watering.iloc[1]}
-            #     )""")
-
-            conn.commit()
+                        ('{watering.iloc[0]}', {int(watering.iloc[1])})
+                        END""")
+                conn.commit()
+            else:
+                print(f"Plant {watering.iloc[1]} isn't in the database!")
 
 
-def upload_recordings_data(df: pd.DataFrame, conn: Connection, config):
+def upload_recordings_data(df: pd.DataFrame, conn: Connection, config, bad_plants):
     """Uploads transaction data to the database."""
-    # Not sure if to_sql will work yet, use INSERT INTO if not.
     recordings = df[['timestamp', 'temp',
                      'soil_moisture', 'botanist_id', 'plant_id']]
     with conn.cursor() as cur:
         for _, record in recordings.iterrows():
-            cur.execute(
-                f"""INSERT INTO {config['SCHEMA_NAME']}.recordings
-                    (timestamp, temp, soil_moisture, botanist_id, plant_id) VALUES
-                    ('{record.iloc[0]}', '{record.iloc[1]}', '{record.iloc[2]}', '{record.iloc[3]}', '{record.iloc[4]}')
-                    )""")
+            if record.iloc[4] not in bad_plants:
+                cur.execute(
+                    f"""INSERT INTO {config['SCHEMA_NAME']}.recording
+                        (timestamp, temp, soil_moisture, botanist_id, plant_id) VALUES
+                        ('{record.iloc[0]}', '{record.iloc[1]}', '{record.iloc[2]}', '{int(record.iloc[3])}', '{int(record.iloc[4])}')
+                        """)
 
-            conn.commit()
+                conn.commit()
+            else:
+                print(f"Plant {record.iloc[4]} isn't in the database!")
 
 
-def load(df: pd.DataFrame) -> None:
+def load(df: pd.DataFrame, config) -> None:
     """main load function"""
-    with connect_to_db(ENV) as conn:
-        df = get_botanist_ids(df, conn, ENV)
-        upload_watering_data(df, conn, ENV)
-        upload_recordings_data(df, conn, ENV)
+    with connect_to_db(config) as conn:
+        bad_plants = check_plants(df, conn, config)
+        df = get_botanist_ids(df, conn, config)
+        upload_watering_data(df, conn, config, bad_plants)
+        upload_recordings_data(df, conn, config, bad_plants)
 
 
 if __name__ == "__main__":
     load_dotenv()
-    # load(pd.DataFrame({'name': ['mickey', 'mickey', 'mouse'], 'phone': [
-    #     '839429', '839429', '4872682'], 'email': ['mickeymouse@clubhouse.com', 'mickeymouse@clubhouse.com', 'anemail@email.com']}))
-    load(pd.DataFrame({"email": ['mickeymouse@clubhouse.com'],
-                      "name": ['mickey'],
-                       "phone": '666',
-                       "license_url": 'license_url',
-                       "origin_url": 'origin_url',
-                       "last_watered": pd.to_datetime('01/01/2024'),
-                       "plant_id": 5,
-                       "timestamp": pd.to_datetime('01/01/2024'),
-                       "soil_moisture": 2.0,
-                       "temp": 3.0}))
+    start_time = time.time()
+    plants = asyncio.run(get_all_plants(51))
+    df = transform(plants)
+    load(df, ENV)
+    print(f"--- {(time.time() - start_time)} seconds taken ---")
