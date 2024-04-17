cce113563e82a22864542a32d5c0d2b8316a7b85
ayesha <ayeshaparvez0603@gmail.com>
2024-04-17 10:38:38 +0100

    speed up by doing asynchronous api calls

--- etl-script/extract.py
+++ etl-script/extract.py
@@ -2,35 +2,42 @@
 The data should then be returned as a Dataframe."""
 
 import pandas as pd
-import requests
-import multiprocessing
+import asyncio
+import aiohttp
+from datetime import datetime
 
 
-def get_plant_data(plant_id: int) -> pd.DataFrame:
+async def get_plant_data(session, plant_id: int) -> pd.DataFrame:
     '''Extracts json data from API endpoint for given plant id.'''
 
     try:
-        response = requests.get(
-            f"https://data-eng-plants-api.herokuapp.com/plants/{plant_id}", timeout=10)
+        response = await session.get(
+            f"https://data-eng-plants-api.herokuapp.com/plants/{plant_id}", timeout=20)
 
-        plant = response.json()
+        data = await response.json()
 
-        return plant
+        data['plant_id'] = plant_id
+
+        return data
 
     except Exception as e:
         return {'error': 'Cannot connect to the API.',
                 'exception': e}
 
 
-def get_all_plants(no_of_plants: int) -> list[dict]:
+async def get_all_plants(no_of_plants: int) -> list[dict]:
     '''Returns a list of plants along with their data.'''
-    pool_obj = multiprocessing.Pool()
 
-    with pool_obj as p:
-        ans = p.map(get_plant_data, range(0, no_of_plants))
-        return ans
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+
+        for i in range(no_of_plants):
+            tasks.append(get_plant_data(session, i))
+
+        return await asyncio.gather(*tasks, return_exceptions=True)
 
 
 if __name__ == "__main__":
 
-    get_all_plants(51)
+    resp = asyncio.run(get_all_plants(51))
+
