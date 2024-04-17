174e003ff1603e3e3f42112787af482d047a2e76
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-17 17:37:00 +0100

    pytest

--- etl-script/test_load.py
+++ etl-script/test_load.py
@@ -1,8 +1,22 @@
 """Test the functions involved in the load file."""
 from unittest.mock import patch
+import pandas as pd
 from load import get_botanist_ids, check_plants, upload_watering_data, upload_recordings_data, load
 
 
-def test_load():
+@patch("load.environ")
+@patch("load.connect_to_db")
+def test_load(fake_db_connection, fake_environ):
     """A test to pass the workflow checks (for now)"""
-    return None
+    fake_db_connection = None
+    fake_db_connection.commit = None
+    fake_db_connection.cursor = None
+    fake_db_connection.cursor().execute.return_value = None
+    fake_db_connection.cursor().fetchone = {'botanist_id': 1}
+    fake_environ = {'SCHEMA_NAME': 'test_schema'}
+    test_return = get_botanist_ids(pd.DataFrame({'name': ['mickey', 'mouse'], 'phone': [
+        '8437234', '0-09280'], 'email': ['87324r832@gmail.com', 'mickeymouse@clubhouse.com']}), fake_db_connection, fake_environ)
+    assert 'botanist_id' in test_return
+    assert isinstance(test_return['botanist_id'].values[0], int)
+    assert test_return == pd.DataFrame({'name': ['mickey', 'mouse'], 'phone': [
+        '8437234', '0-09280'], 'email': ['87324r832@gmail.com', 'mickeymouse@clubhouse.com'], 'botanist_id': [1, 1]})
