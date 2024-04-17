cd5fbf217e7ded3f5212aeb619c70a57f030bee5
howardman <howardppg@gmail.com>
2024-04-17 10:05:16 +0100

    1st draft completed

--- long-term-storage/storage_load.py
+++ long-term-storage/storage_load.py
@@ -1,7 +1,8 @@
 """
-This script is to extract and remove the out-of-date data from the database 
- it into long-term storage in an S3 bucket.
+This script is to extract and remove the out-of-date data from the database, and 
+transport it into long-term storage in an S3 bucket, where it is stored in CSV files.
 """
+from datetime import datetime
 from os import environ as ENV
 
 from boto3 import client
@@ -22,31 +23,78 @@ def connect_to_db(config) -> Connection:
     )
 
 
-def get_recent_entries(conn: Connection) -> None:
-    """Goes into database, and pulls all entries in the plant table which have
-    a recording older than 24 hours. Returns a CSV file."""
+def get_recent_recordings(conn: Connection, current_timestamp: datetime,
+                          config) -> pd.DataFrame:
+    """Goes into database, and pulls all entries in the recording table with a timestamp
+    older than 24 hours."""
     with conn.cursor() as cur:
-        cur.execute("""SELECT p.* FROM s_alpha.plant AS p
-                    JOIN s_alpha.recordings AS r
-                    ON (r.plant_id = p.plant_id)
-                    WHERE DATEDIFF(hour, CURRENT_TIMESTAMP, r.timestamp) > 24;""")
-        print(cur.fetchall())
+        cur.execute(f"""SELECT r.* 
+                    FROM {config["SCHEMA"]}.recording AS r
+                    WHERE DATEDIFF(hour, %s, r.timestamp) > 24;""",
+                    (current_timestamp,))
+
+        result = cur.fetchall()
+
+    conn.commit()
+    cur.close()
+
+    return pd.DataFrame(result)
 
 
-def load_csv_file() -> None:
-    """Given our recent entries, we now connect to an S3 bucket and load."""
-    pass
+def del_recent_recordings(conn: Connection, current_timestamp: datetime, config) -> None:
+    """Goes into database, and deletes all entries in the recording table with a timestamp
+    older than 24 hours."""
+    with conn.cursor() as cur:
+        cur.execute(f"""DELETE r.*
+                    FROM {config["SCHEMA"]}.recording AS r
+                    WHERE DATEDIFF(hour, %s, r.timestamp) > 24;""",
+                    (current_timestamp,))
+
+    conn.commit()
+    cur.close()
 
 
-def storage_load():
-    """To be solely run when executing this Python script."""
-    pass
+def create_current_datetime_filename(current_timestamp: datetime) -> str:
+    """Given the current timestamp, a CSV filename is created."""
+    current_yr = current_timestamp.year()
+    current_mth = current_timestamp.month()
+    current_day = current_timestamp.day()
+    current_hr = current_timestamp.hour()
+    current_min = current_timestamp.minute()
+
+    return f"{current_yr}/{current_mth}/{current_day}/{current_hr}:{current_min}"
+
+
+def convert_data_csv_file(data: pd.DataFrame, csv_filename: str) -> None:
+    """Given a list of dict objects, we convert it into CSV format."""
+    data.to_csv(csv_filename, index=False)
+
+
+def load_csv_file(s3_client: client, csv_filename: str, config) -> None:
+    """Given a CSV file, we now connect to an S3 bucket and load."""
+    s3_client.upload_file(Bucket=config["BUCKET_STORAGE_NAME"],
+                          Filename=csv_filename,
+                          Key=csv_filename)
 
 
 if __name__ == "__main__":
+
+    current_timestamp = datetime.now()
 
     load_dotenv()
 
     conn = connect_to_db(ENV)
 
-    print(get_recent_entries(conn))
+    old_recordings = get_recent_recordings(conn, current_timestamp)
+
+    del_recent_recordings(conn, current_timestamp, ENV)
+
+    csv = f"{create_current_datetime_filename(current_timestamp)}.csv"
+
+    convert_data_csv_file(old_recordings, csv)
+
+    s3 = client("s3",
+                aws_access_key_id=ENV["AWS_ACCESS_KEY_ID"],
+                aws_secret_access_key=ENV["AWS_SECRET_ACCESS_KEY"])
+
+    load_csv_file(s3, csv, ENV)
