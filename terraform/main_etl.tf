3fb285d6010ea4aae24054f72e97a70d53e9260a
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-18 12:01:22 +0100

    gitignore

--- terraform/main_etl.tf
+++ terraform/main_etl.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "assume_role_event" {
       identifiers = ["scheduler.amazonaws.com"]
     }
 
-    actions = ["sts:AssumeRole", "lambda:InvokeFunction"]
+    actions = ["sts:AssumeRole"]
   }
 }
 
