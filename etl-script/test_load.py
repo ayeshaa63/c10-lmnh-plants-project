1c95a4ef68c77a2fb02a633113cb947e12ba4d36
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-18 09:13:05 +0100

    print test

--- etl-script/test_load.py
+++ etl-script/test_load.py
@@ -4,13 +4,14 @@ import pandas as pd
 from load import get_botanist_ids, check_plants, upload_watering_data, upload_recordings_data, load
 
 
-def test_Call_sql():
+def test_botanist_id_func():
     mock_connect = MagicMock()
     cursor = mock_connect.cursor.return_value
     cursor.execute.return_value.fetchone.side_effect = {'botanist_id': 1}
     fake_environ = {'SCHEMA_NAME': 'test_schema'}
     test_return = get_botanist_ids(pd.DataFrame({'name': ['mickey', 'mouse'], 'phone': [
         '8437234', '0-09280'], 'email': ['87324r832@gmail.com', 'mickeymouse@clubhouse.com']}), mock_connect, fake_environ)
+    print(test_return)
     assert 'botanist_id' in test_return
     assert pd.api.types.is_numeric_dtype(test_return['botanist_id'])
     assert test_return == pd.DataFrame({'name': ['mickey', 'mouse'], 'phone': [
