329d696ef500ffe535a08bc5f6cbec9a93df4099
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-18 11:49:51 +0100

    invoke lambda

--- terraform/main_etl.tf
+++ terraform/main_etl.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "assume_role_event" {
       identifiers = ["scheduler.amazonaws.com"]
     }
 
-    actions = ["sts:AssumeRole"]
+    actions = ["sts:AssumeRole", "lambda:InvokeFunction"]
   }
 }
 
