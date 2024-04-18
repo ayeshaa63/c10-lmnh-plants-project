b7ee548ddcdfc5e31a0ff6a9c8af153950780fb1
NMcKittie <nathankit66@gmail.com>
2024-04-18 09:38:47 +0100

    updating gitignore

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "AWS_ACCESS_KEY_ID" {
+    type = string
+}
+variable "AWS_SECRET_ACCESS_KEY_ID" {
+    type = string
+}
+variable "DB_USERNAME" {
+    type = string
+    default = "postgres"
+}
+variable "DB_PASSWORD" {
+    type = string
+}
