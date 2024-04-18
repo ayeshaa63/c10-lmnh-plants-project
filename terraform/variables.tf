4297f7b36aa2c42530c8ea56f691b244ac149ac8
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-18 10:02:29 +0100

    variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,10 +4,22 @@ variable "AWS_ACCESS_KEY_ID" {
 variable "AWS_SECRET_ACCESS_KEY_ID" {
     type = string
 }
-variable "DB_USERNAME" {
+variable "DB_USER" {
     type = string
     default = "postgres"
 }
-variable "DB_PASSWORD" {
+variable "DB_PASS" {
     type = string
+}
+variable "DB_HOST" {
+    type = string
+}
+variable "DB_NAME" {
+    type = string
+}
+variable "DB_PORT" {
+    type = string
+}
+variable "SCHEMA_NAME" {
+    type = string 
 }
