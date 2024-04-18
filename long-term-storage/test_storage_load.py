f09fdd36a4bd95309737677d70d1386b74d33d58
howardman <howardppg@gmail.com>
2024-04-18 11:22:42 +0100

    Fixed bug with test

--- long-term-storage/test_storage_load.py
+++ long-term-storage/test_storage_load.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import pandas as pd
 import pytest
 
-from storage_load import create_current_datetime_filename
+from storage_load import create_current_datetime_key
 
 
 @pytest.fixture
@@ -10,6 +10,6 @@ def sample_datetime():
     return datetime(year=2000, month=11, day=29, hour=14, minute=48)
 
 
-def test_create_current_datetime_filename(sample_datetime):
-    assert create_current_datetime_filename(
+def test_create_current_datetime_key(sample_datetime):
+    assert create_current_datetime_key(
         sample_datetime) == "2000/11/29/14:48"
