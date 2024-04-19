9cc8c72ef5c06852dad844eeca02c732df373763
Dana <trainee.dana.weetman@sigmalabs.co.uk>
2024-04-19 10:24:31 +0100

    main

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pymssql import connect
 from dotenv import load_dotenv
 import streamlit as st
+from vega_datasets import data
 
 
 def connect_to_db(config):
@@ -19,11 +20,11 @@ def connect_to_db(config):
     )
 
 
-def get_data_from_db(conn, config) -> pd.DataFrame:
+def get_data_from_db(conn, config, table_name) -> pd.DataFrame:
     '''Returns a transactions as DataFrame from database.'''
     with conn.cursor() as cur:
 
-        cur.execute(f"SELECT * FROM {config['SCHEMA_NAME']}.recording;")
+        cur.execute(f"SELECT * FROM {config['SCHEMA_NAME']}.{table_name};")
 
         rows = cur.fetchall()
 
@@ -32,42 +33,90 @@ def get_data_from_db(conn, config) -> pd.DataFrame:
     return df
 
 
+def get_table_data(conn, config) -> pd.DataFrame:
+    '''Returns a transactions as DataFrame from database.'''
+    with conn.cursor() as cur:
+
+        cur.execute(f"""SELECT p.name as 'Plant', r.timestamp as 'Time', r.temp as 'Temperature', r.soil_moisture as 'Soil moisture' 
+                    FROM {config['SCHEMA_NAME']}.recording as r
+                    JOIN {config['SCHEMA_NAME']}.plant as p
+                    ON (r.plant_id = p.plant_id);""")
+
+        rows = cur.fetchall()
+
+        df = pd.DataFrame.from_dict(rows)
+
+    return df
+
+
+def world_map(origin_data):
+
+    countries = alt.topo_feature(data.world_110m.url, 'countries')
+    background = alt.Chart(countries).mark_geoshape(
+        fill='lightgray',
+        stroke='white',
+        tooltip=None
+    ).project('equirectangular').properties(
+        width=500,
+        height=400
+    )
+    points = alt.Chart(origin_data).mark_circle(color='red', size=300).encode(
+        longitude='lat:Q',
+        latitude='long:Q',
+        size=alt.value(10),
+        tooltip='location_name'
+    ).project(
+        "equirectangular"
+    ).properties(
+        width=500,
+        height=400
+    )
+    return background + points
+
+
 if __name__ == "__main__":
 
     load_dotenv()
 
-    conn = connect_to_db(ENV)
-    data = get_data_from_db(conn, ENV)
+    with connect_to_db(ENV) as conn:
+        record_data = get_data_from_db(conn, ENV, 'recording')
+        basic_stats = get_table_data(conn, ENV)
 
-    st.title('LMNH Plants Dashboard')
+        st.title('LMNH Plants Dashboard')
 
-    st.write(data)
+        with st.sidebar:
+            plant_list = st.multiselect(
+                'Plant ID', basic_stats['Plant'], basic_stats['Plant'])
 
-    average_temps = data.groupby(['plant_id'])['temp'].mean().reset_index()
+        st.write(basic_stats[basic_stats['Plant'].isin(plant_list)])
 
-    avg_temp = alt.Chart(average_temps, title='Average Temperatures').mark_bar().encode(
-        x='plant_id:N',
-        y='temp',
-        color='plant_id:N'
-    )
+        average_temps = basic_stats.groupby(
+            ['Plant'])['Temperature'].mean().reset_index()
 
-    st.altair_chart(avg_temp)
+        avg_temp = alt.Chart(average_temps[average_temps['Plant'].isin(plant_list)], title='Average Temperatures').mark_bar().encode(
+            x='Plant:N',
+            y='Temperature',
+            color='Plant:N'
+        )
 
-    with st.sidebar:
-        plant_list = st.multiselect('Plant ID', range(51), range(51))
+        st.altair_chart(avg_temp)
 
-    temps = alt.Chart(data[data['plant_id'].isin(plant_list)], title='Temperature over time').mark_line().encode(
-        x='timestamp:T',
-        y='temp:Q',
-        color='plant_id:N'
-    )
+        temps = alt.Chart(basic_stats[basic_stats['Plant'].isin(plant_list)], title='Temperature over time').mark_line().encode(
+            x='Time:T',
+            y='Temperature:Q',
+            color='Plant:N'
+        )
 
-    st.altair_chart(temps, use_container_width=True)
+        st.altair_chart(temps, use_container_width=True)
 
-    moist = alt.Chart(data.loc[data['plant_id'].isin(plant_list)], title='Soil moisture over time').mark_line().encode(
-        x='timestamp:T',
-        y='soil_moisture:Q',
-        color='plant_id:N'
-    )
+        moist = alt.Chart(basic_stats.loc[basic_stats['Plant'].isin(plant_list)], title='Soil moisture over time').mark_line().encode(
+            x='Time:T',
+            y='Soil Moisture:Q',
+            color='Plant:N'
+        )
 
-    st.altair_chart(moist, use_container_width=True)
+        st.altair_chart(moist, use_container_width=True)
+
+        w_map = world_map(get_data_from_db(
+            connect_to_db(ENV), ENV, 'origin'))
+        st.altair_chart(w_map, use_container_width=True)
